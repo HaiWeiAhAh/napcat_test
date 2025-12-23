989728eb4eeba024bb8dff84285fe1b52d108141
haiweipc <3525843539@qq.com>
2025-12-23 16:59:12 +0800

    测试戳一戳功能的代码

--- hello world.py
+++ hello world.py
@@ -1,9 +1,26 @@
+# -*- coding: utf-8 -*-
 import requests
+import http.client
+import json
 
-url = "http://172.17.0.1:8099/send_private_msg"
+"""url = "http://172.17.0.1:8099/send_private_msg"
 payload = {
     "user_id": 3525843539,
     "message": "你好，NapCatQQ！"
+}"""
+#resp = requests.post(url, json=payload)
+#print(resp.json())
+
+
+
+conn = http.client.HTTPSConnection("http://172.17.0.1:8099")
+payload = json.dumps({
+   "user_id": 3525843539,
+})
+headers = {
+   'Content-Type': 'application/json'
 }
-resp = requests.post(url, json=payload)
-print(resp.json())
+conn.request("POST", "/send_poke", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
