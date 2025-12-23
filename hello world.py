42fee02a9b48d838ad314c99c1c1427b1f30f47d
haiweipc <3525843539@qq.com>
2025-12-23 17:23:18 +0800

    测试戳一戳功能的代码修正

--- hello world.py
+++ hello world.py
@@ -13,7 +13,7 @@ payload = {
 
 
 
-conn = http.client.HTTPSConnection("172.17.0.1，",9099)
+conn = http.client.HTTPSConnection("172.17.0.1",9099)
 payload = json.dumps({
    "user_id": 3525843539,
 })
