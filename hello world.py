78d162bc7758bc5f321ca14a7e1801a33d05efbe
haiweipc <3525843539@qq.com>
2025-12-24 13:18:37 +0800

    添加了群全体成员禁言功能和测试监听wb服务器的推送消息

--- hello world.py
+++ hello world.py
@@ -2,7 +2,7 @@
 import http.client
 import json
 import payload_templates
-
+import websockets
 def post_request(url_api,raw_payload,method:str = "POST"):
     try:
         init_payload = json.dumps(raw_payload)
@@ -16,10 +16,18 @@ def post_request(url_api,raw_payload,method:str = "POST"):
     except Exception as e:
         print(e)
 
+async def connect_to_napcat():
+    async with websockets.connect("ws://localhost:6099") as websocket:
+        # 持续监听 Napcat 推送的消息
+        async for raw_message in websocket:
+            # 拿到原始消息后，交给 RecvHandler 处理
+            print(raw_message)
+    #conn = http.client.HTTPConnection("172.17.0.1", 9099)
+
 if __name__ == '__main__':
     conn = http.client.HTTPConnection("172.17.0.1", 9099)
     while True:
-        raw_input = input("选择功能【1.发送群聊消息】【2.发送私聊消息】:")
+        raw_input = input("选择功能【1.发送群聊消息】【2.发送私聊消息】【3.全体禁言】:")
         if raw_input == "1":
             group_id = input("发送到群：")
             msg = input(":")
@@ -34,5 +42,17 @@ if __name__ == '__main__':
             url_api = "/send_private_msg"
             #
             post_request(url_api, payload)
+        elif raw_input == "3":
+            payload = payload_templates.set_group_whole_ban()
+            url_api = "/set_group_whole_ban"
+            #
+            post_request(url_api, payload)
+        elif raw_input == "4":
+            while True:
+                command = input("监听服务器的推送：")
+                if command == "exit":
+                    break
+                connect_to_napcat()
         else:
             continue
+
